2a773f18f70a9c1c70a6175417dd52c611cd2e1a
unknown <786muhammedashfaq@gmail.com>
2024-05-18 16:44:35 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AddScenarioForm from './components/AddScenarioForm';
+import AddVehiclesForm from './components/AddVehiclesForm';
+import AllScenarios from './components/AllScenarios';
+import Home from './components/Home';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+toast.configure();
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path='/addscenario' element={<AddScenarioForm/>} />
+          <Route path='/allscenarios' element={<AllScenarios/>} />
+          <Route path='/addvehiclesform' element={<AddVehiclesForm/>} />
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ActivePageProvider from './contexts/ActivePageProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ActivePageProvider>
+
+
     <App />
+    </ActivePageProvider>
   </React.StrictMode>
 );
 
